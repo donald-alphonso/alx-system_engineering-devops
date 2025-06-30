de504afba25339bf0b6a11fce2265fdbc6827a35
donald.ahossi <donaldalphonso11@gmail.com>
2025-06-30 19:11:52 +0200

    Fix Task 4: Puppet script to configure /etc/ssh/ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
+# This Puppet script configures SSH to use a private key and disable password authentication
 file_line { 'Declare identity file':
-  path  => '/home/ubuntu/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => '    IdentityFile ~/.ssh/school',
   match => '^\\s*IdentityFile',
 }
 
 file_line { 'Turn off passwd auth':
-  path  => '/home/ubuntu/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => '    PasswordAuthentication no',
   match => '^\\s*PasswordAuthentication',
 }
